22fdec6db7a423840f5422261915ceae8096e8be
amni1407 <uyulmeni1407@gmail.com>
2024-07-19 19:11:51 +0800

    init

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php require_once 'admin/db_con.php'; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>PENDAFTARAN MASUK TABIKA KEMAS RAUDHATUL AISY</title>
+  </head>
+  <body>
+    <div class="container"><br>
+      <a class="btn btn-primary float-right" href="admin/login.php">Login</a>
+          <h1 class="text-center">JOM MASUK TABIKA !</h1>
+          <h1 class="text-center">TABIKA KEMAS RAUDHATUL AISY</h1></br>
+
+          
+        <br>
+        <?php if (isset($_POST['showinfo'])) {
+          $choose= $_POST['choose'];
+          $roll = $_POST['roll'];
+          if (!empty($choose && $roll)) {
+            $query = mysqli_query($db_con,"SELECT * FROM `student_info` WHERE `roll`='$roll' AND `class`='$choose'");
+            if (!empty($row=mysqli_fetch_array($query))) {
+              if ($row['roll']==$roll && $choose==$row['class']) {
+                $stroll= $row['roll'];
+                $stname= $row['name'];
+                $stclass= $row['class'];
+                $city= $row['city'];
+                $photo= $row['photo'];
+                $pcontact= $row['pcontact'];
+              ?>
+        <div class="row">
+          <div class="col-sm-6 offset-sm-3">
+            <table class="table table-bordered">
+              <tr>
+                <td rowspan="5"><h3>Student Info</h3><img class="img-thumbnail" src="admin/images/<?= isset($photo)?$photo:'';?>" width="250px"></td>
+                <td>Name</td>
+                <td><?= isset($stname)?$stname:'';?></td>
+              </tr>
+              <tr>
+                <td>Roll</td>
+                <td><?= isset($stroll)?$stroll:'';?></td>
+              </tr>
+              <tr>
+                <td>Class</td>
+                <td><?= isset($stclass)?$stclass:'';?></td>
+              </tr>
+              <tr>
+                <td>City</td>
+                <td><?= isset($city)?$city:'';?></td>
+              </tr>
+              <tr>
+                <td>Submit Date</td>
+                <td><?= isset($pcontact)?$pcontact:'';?></td>
+              </tr>
+            </table>
+          </div>
+        </div>  
+      <?php 
+          }else{
+                echo '<p style="color:red;">Please Input Valid Roll & Email</p>';
+              }
+            }else{
+              echo '<p style="color:red;">Your Input Doesn\'t Match!</p>';
+            }
+            }else{?>
+              <script type="text/javascript">alert("Data Not Found!");</script>
+            <?php }
+          }; ?>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
